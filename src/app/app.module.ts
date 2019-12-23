8623e7586f5103de6b2b9e0b6a03f960672ddd38
Andy <andyem64@gmail.com>
2019-12-23 18:18:22 -0500

    wireframe

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HeadComponent } from './head/head.component';
+import { PlanComponent } from './plan/plan.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HeadComponent,
+    PlanComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
